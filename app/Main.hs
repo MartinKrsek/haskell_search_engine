1242607a0d9e8112d643d86f8309c4d2f3b71ef8
Fullajtar <fullajtar.dominik@gmail.com>
2022-05-20 18:29:31 +0200

    feat: creating inversed index and writing  to inverseIndex.json

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,10 @@ import qualified Data.ByteString.Lazy as B
 import Lib
 import Text.Regex.TDFA
 import Reader
+import Inverser
 
 main :: IO ()
 main = do 
-    readMyFile "archive/testBig.txt"
+    -- readMyFile "archive/testBig.txt"
+    inverse
     putStr "Finished successfully!"
