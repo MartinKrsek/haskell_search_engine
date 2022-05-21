21169c28b2211bc0f236a78530c9b55f3568ba7e
Fullajtar <fullajtar.dominik@gmail.com>
2022-05-21 15:40:02 +0200

    feat: User search returns page indexes

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,11 @@ import qualified Data.ByteString.Lazy as B
 import Lib
 import Reader
 import Inverser
+import UserIO
 
 main :: IO ()
 main = do 
     -- readMyFile "archive/testBig.txt"
-    inverse
+    -- inverse
+    search
     putStr "Finished successfully!"
