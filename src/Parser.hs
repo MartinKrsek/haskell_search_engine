9b7bc790af413f99e56d62bc107dfb4ee055dc8b
martin-krsek <martin.krsek1@gmail.com>
2022-05-22 17:54:18 +0200

    feature: remove short words, code cleanup

--- src/Parser.hs
+++ src/Parser.hs
@@ -20,7 +20,7 @@ parse :: Integer -> String -> IO ()
 parse id html = do
   src <- parseTags <$> stringToIoString html
   let title = myMap src "<title>"
-  let metaContent = concat $ map words $ map getContent $ sections (~== "<meta name=\"description\">") src
+  let metaContent = myMetaContent src
   let h1 = myMap src "<h1>"
   let h2 = myMap src "<h2>"
   let h3 = myMap src "<h3>"
@@ -31,12 +31,15 @@ parse id html = do
   let span = myMap src "<span>"
   let div = myMap src "<div>"
   writeParsedFile id (nub $ title ++ metaContent ++ h1 ++ h2 ++ h3 ++ h4 ++ h5 ++ h6 ++ imgAlt ++ span ++ div)
-  where getContent = fromAttrib "content" . head . filter isTagOpen
-        getAlt = fromAttrib "alt" . head . filter isTagOpen
+  where getContent = dequote . unwords . words . map toLower . removeNonLetters . filter isAscii . fromAttrib "content" . head . filter isTagOpen
+        getAlt = dequote . unwords . words . map toLower . removeNonLetters . filter isAscii . fromAttrib "alt" . head . filter isTagOpen
         removeEmpty = filter (not . null)
-        myImgMap src = concat $ map words $ removeEmpty $ map getAlt $ sections (~== "<img>") src
-        clear = dequote . unwords . words . map toLower . filter (`notElem` "{},.?!-:;\"\'|"). filter isAscii . fromTagText . head . filter isTagText
-        myMap src tag = concat $ map words $ removeEmpty $ map clear $ sections (~== tag) src
+        removeShort = filter ((> 2) . length) 
+        removeNonLetters = filter (`notElem` "{},.?!+*%$#@^&_+=`~-:;\"\'|")
+        myMetaContent src = removeShort $ concat $ map words $ removeEmpty $ map getContent $ sections (~== "<meta name=\"description\">") src
+        myImgMap src = removeShort $ concat $ map words $ removeEmpty $ map getAlt $ sections (~== "<img>") src
+        clear = dequote . unwords . words . map toLower . removeNonLetters . filter isAscii . fromTagText . head . filter isTagText
+        myMap src tag = removeShort $ concat $ map words $ removeEmpty $ map clear $ sections (~== tag) src
         dequote ('\"':xs) | last xs == '\"' = init xs
         dequote x = x
 
